f94dedf78958cff8a3f5a7f4b51c3a5277513e71
jimmycababa <jimmycababa@gmail.com>
2021-10-14 21:54:07 -0500

    user model changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,7 +28,7 @@ test "email should not be too long" do
   assert_not @user.valid?
 end
 
-test "email validation should reject ivalid addresses" do
+test "email validation should reject invalid addresses" do
   invalid_addresses = %w[user@example,com user_at_foo.org
     user.name@example.
     foo@bar_baz.com foo@bar+baz.com]
@@ -37,4 +37,10 @@ test "email validation should reject ivalid addresses" do
     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
 end
 end
-end 
+test "email addresses should be unique" do
+  duplicate_user = @user.dup
+  duplicate_user.email = @user.email.upcase
+  @user.save
+  assert_not duplicate_user.valid?
+end
+end
