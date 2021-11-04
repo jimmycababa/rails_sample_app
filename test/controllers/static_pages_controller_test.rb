bd0036ae70e58720367a2dbb0bc673401d146f8a
jimmycababa <jimmycababa@gmail.com>
2021-11-04 11:15:05 -0500

    added tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get static_pages_root_url
-    assert_response :success
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
