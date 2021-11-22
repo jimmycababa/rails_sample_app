753c3ca60ff52f988efc5287f0a141d60e241787
jimmycababa <jimmycababa@gmail.com>
2021-11-22 12:51:49 -0600

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,11 @@
 module ApplicationHelper
-<<<<<<< HEAD
     # Returns the full title on a per-page basis
-=======
-    # returns the full title on a per-page basis
->>>>>>> rails-flavored-ruby
     def full_title(page_title = '')
         base_title = "Ruby on Rails Tutorial Sample App"
         if page_title.empty?
             base_title
         else
-<<<<<<< HEAD
             page_title + ' | ' + base_title
-=======
-            "#{page_title} | #{base_title}"
->>>>>>> rails-flavored-ruby
         end
     end
 end
